c0c53c8d817e07773837c9b31224e62746894702
Krish Kalla <116767392+Buddhimaan1026@users.noreply.github.com>
2023-05-09 00:04:02 -0400

    Working Code to Connect to Canvas & Parse Data

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-url = "https://canvas.exeter.edu/api/v1/courses?per_page=100";
+canvasURL = "https://canvas.exeter.edu/api/v1/courses?per_page=100";
 
 const options = {
     method: "GET",
@@ -7,16 +7,49 @@ const options = {
     },
 }
 
-var array = new Array();
+fetch(canvasURL, options)
+    .then( response => response.json())
+    .then( response => getCurrentCourses(response))
+    .then( response => getCourseLinks(response))
 
-fetch(url, options)
-    .then(response => response.json())
-    .then(response => load(response))
-    .then(response => console.log(JSON.stringify(response)))
 
-function load(response) {
+
+
+
+
+function getCurrentCourses(response) {
+    const filteredResponse = response.filter(course => !course.access_restricted_by_date);
+    console.log(filteredResponse);
+    return filteredResponse;
+}
+
+function getCourseNames(response) {
     for (let i = 0; i < response.length; i++) {
-        console.log(typeof response);
+        console.log(response[i].name)
+    }
+    return response;
+}
+
+function getCourseCodes(response) {
+    for (let i = 0; i < response.length; i++) {
+        console.log(response[i].course_code)
+    }
+    return response;
+}
+
+function getCourseID(response) {
+    for (let i = 0; i < response.length; i++) {
+        console.log(response[i].id)
+    }
+    return response;
+}
+
+function getCourseLinks(response) {
+    for (let i = 0; i < response.length; i++) {
+        let id = (response[i].id)
+        let courseName = (response[i].name)
+        let string = courseName + ': ' + 'https://canvas.exeter.edu/courses/' + id;
+        console.log(string)
     }
     return response;
 }
