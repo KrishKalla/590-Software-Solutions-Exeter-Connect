f0ac94b227633c80271901303faee52b384ce5fa
Krish Kalla <116767392+Buddhimaan1026@users.noreply.github.com>
2023-05-08 22:15:50 -0400

    Edits to JS

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+import fetch from node-fetch
+
 url = "https://canvas.exeter.edu/api/v1/courses?per_page=9999";
 let response = [];
 
@@ -9,22 +11,6 @@ const options = {
     },
 };
 
+
 fetch (url, options)
-    // .then( response => response.json())
-    // .then( response => console.log(JSON.stringify(response)))
-    .then(async(response) => {
-        let clone = response.clone();
-        let res = await clone.json();
-        console.log(res);
-        return response.blob()
-    })
-    .then(blob => {
-        var url = window.URL.createObjectURL(blob);
-        var a = document.createElement('a');
-        a.href = url;
-        a.download = "jsonfile.txt";
-        a.click();
-    })
-    .catch(function(err) {
-        console.error(err);
-    })
+  
