9f46717c17ba97b30ab5ce177d7a584272ec9589
EthanDC80 <89340600+EthanDC80@users.noreply.github.com>
2023-05-12 00:36:59 -0400

    Created functions that prints the subject, title, description, and due date

--- javascript.js
+++ javascript.js
@@ -14,55 +14,50 @@ fetch(canvasURL, options)
     .then( response => getCurrentCourses(response))
     .then( response => getCourseLinks(response))
 
-
-
-
-
-
-
 function getCurrentCourses(response) {
     const filteredResponse = response.filter(course => !course.access_restricted_by_date);
-    console.log(filteredResponse);
+    // console.log(JSON.stringify(filteredResponse));
     return filteredResponse;
-}
-
-function getCourseNames(response) {
-    numCourses = response.length;
-    for (let i = 0; i < response.length; i++) {
-        console.log(response[i].name)
-    }
-    return response;
-}
-
-function getCourseCodes(response) {
-    for (let i = 0; i < response.length; i++) {
-        console.log(response[i].course_code)
-    }
-    return response;
-}
-
-function getCourseID(response) {
-    for (let i = 0; i < response.length; i++) {
-        console.log(response[i].id)
-    }
-    return response;
 }
 
 function getCourseLinks(response) {
     for (let i = 0; i < response.length; i++) {
-        let id = (response[i].id)
-        let courseName = (response[i].name)
-        let webString = 'https://canvas.exeter.edu/api/v1/courses/' + id + '/assignments?per_page=999';
+        let id = (response[i].id);
+        let courseName = (response[i].name);
+        let webString = 'https://canvas.exeter.edu/api/v1/courses/' + id + '/assignments?per_page=100';
         let consoleString = courseName + ': ' + 'https://canvas.exeter.edu/courses/' + id;
-        getCourseInfo(webString);
-        console.log(webString);
-        console.log(consoleString);
+        getCourseInfo(webString, courseName);
+        // console.log(consoleString);
     }
     return response;
 }
 
-function getCourseInfo (webString) {
+function getCourseInfo (webString, course_name) {
     fetch(webString, options) 
         .then( response => response.json())
-        .then( response => console.log(response))
+        .then( response => getAssignmentInfo(response, course_name))
+        // .then( response => console.log(JSON.stringify(response)))
+}
+
+// Gets Info for assignments such as subject, description, due date, and the course name
+function getAssignmentInfo (response, course_name) {
+
+    for (let i = 0; i < response.length; i++) {
+        let subject = (course_name);
+        let title = (response[i].name);
+        if(subject == 'undefined')
+            continue;
+        let desc = removeHTML(response[i].description);
+        if(desc == 'undefined')
+            desc = '';
+        let dueDate = (response[i].due_at);
+        console.log(subject + '\n' + title + '\n' + desc + '\n' + dueDate + '\n');
+    }
+    return response;
+}
+
+// Returns certain HTML tags and ornaments
+function removeHTML (HTMLstr){
+    HTMLstr = HTMLstr.replace(/<[^>]+>/g, '').replace(/&nbsp;/g, ' ').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&').replace(/&quot;/g, '\"').replace(/&apos;/g, '\''); // Removes tags such as <p> and </p>
+    return HTMLstr;
 }
