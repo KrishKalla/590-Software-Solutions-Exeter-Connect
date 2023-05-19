2758051ee7b326f1916989ff7c7042f2022b9d67
Krish Kalla <krish.k.2626@gmail.com>
2023-05-19 09:30:53 -0400

    Microsoft Graph Outlook Tutorial Code

---
+++ graphSettings.js
@@ -0,0 +1,11 @@
+const settings = {
+    'clientId': 'bcad00d2-32d2-49fa-a7f6-ae61448b587c',
+    'tenantId': 'b8710300-e17a-4f12-92c5-869695e07115',
+    'graphUserScopes': [
+      'user.read',
+      'mail.read',
+      'mail.send'
+    ]
+  };
+  
+  module.exports = settings;
